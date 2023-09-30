6b9b02900cd6008204854555d365a7d0bcdc7af2
Vyom Fadia <vyom.fadia.work@gmail.com>
2023-09-30 12:05:05 +0100

    feat: implement primes method w/ error handling

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Required number of primes must be greater than 0")
+
+    primes = []
+
+    i = 2
+    while len(primes) < number_of_primes:
+        if all([i % x != 0 for x in primes]):
+            primes.append(i)
+
+        i += 1
+
+    return primes
